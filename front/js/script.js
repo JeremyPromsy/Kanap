65f5c1744b852a7464a2be9cf2c516f66ea01a0e
JeremyPromsy <jeremypromsy@gmail.com>
2023-01-11 16:57:28 +0100

    Finalisation page Accueil

---
+++ front/js/script.js
@@ -0,0 +1,52 @@
+url = "http://localhost:3000/api/products";
+
+ function ajax(url) {
+   fetch(url)
+     .then(function (response) {
+       if (response.ok) {
+         return response.json();
+       }
+     })
+     .then(function (articles) {
+       for (let article of articles) {
+         addItem(
+           article._id,
+           article.imageUrl,
+           article.altTxt,
+           article.name,
+           article.description,
+         );
+       }
+     })
+     .catch((error) => {
+        console.error('Error:', error);
+    })
+ }
+ ajax(url);
+
+ function addItem(idItem, imageUrl, imageAlt, name, description) {
+    var items = document.getElementById("items");
+  
+    let linkItem = document.createElement("a");
+    linkItem.href = "product.html?id=" + idItem;
+    items.append(linkItem);
+  
+    let articleItem = document.createElement("article");
+    linkItem.append(articleItem);
+  
+    let imageItem = document.createElement("img");
+    imageItem.src = imageUrl;
+    imageItem.alt = imageAlt;
+    articleItem.append(imageItem);
+  
+    let titleItem = document.createElement("h3");
+    titleItem.textContent = name;
+    titleItem.classList.add("productName");
+    articleItem.append(titleItem);
+  
+    let descriptionItem = document.createElement("p");
+    descriptionItem.textContent = description;
+    descriptionItem.classList.add("productDescription");
+    articleItem.append(descriptionItem);
+  }
+ 
