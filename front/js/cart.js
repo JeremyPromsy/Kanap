c17aa288167ddb7cb0b012ce4870f9c991cecc3f
JeremyPromsy <jeremypromsy@gmail.com>
2023-02-03 07:55:59 +0100

    MAJ

--- front/js/cart.js
+++ front/js/cart.js
@@ -161,22 +161,6 @@ function getArticle() {
 
 
 
-  function modifyQuantity() {
-    let boutonsQuantity = document.getElementsByClassName("itemQuantity");
-    for (let boutonQuantity of boutonsQuantity) {
-        boutonQuantity.addEventListener('change', function () {
-        let productLocalStorage = localStorage.getItem("panier");
-        let panier = JSON.parse(productLocalStorage);
-  
-
-
-
-        localStorage.setItem("panier", JSON.stringify(panier));
-        getArticle();
-      });
-    }
-  }
-
 
 
   
